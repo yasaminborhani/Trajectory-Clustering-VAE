d2986555e4159f79b63875c65a6476daafd194bf
yasamiborhani <borhaniyasamin@gmail.com>
2023-08-10 17:35:11 +0000

    minor errors are handled

--- datasets/agroverse/data_loader.py
+++ datasets/agroverse/data_loader.py
@@ -62,7 +62,7 @@ def tf_pipeline_gen(cfg):
         tuple: Normalization parameters for data.
     """
     train_dataset = tf.data.TextLineDataset([cfg.Train.meta_data])
-    train_dataset = train_dataset.map(lambda x: parse_text(cfg=cfg), num_parallel_calls=tf.data.AUTOTUNE)
+    train_dataset = train_dataset.map(lambda x: parse_text(x, cfg=cfg), num_parallel_calls=tf.data.AUTOTUNE)
 
     num_samples = train_dataset.reduce(tf.constant(0), lambda acc, _: count_samples(acc, _))
     normalization_params = param_extractor(train_dataset, cfg)
